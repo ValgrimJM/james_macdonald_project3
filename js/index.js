e6705538c8bbba4eea2ecd8668e0ffb7603a3b73
James MacDonald <jamesdavidmacdonald@gmail.com>
2018-08-15 09:51:14 -0400

    Horizontal Accordion working

--- js/index.js
+++ js/index.js
@@ -68,7 +68,12 @@ const time_periods = [
 // *************Array of Search Terms *****************
 const base_names =["Chordata,Arthropoda", "Chordata,Arthropoda", "Chordata", "Tetrapoda", "Tetrapoda", "Tetrapoda", "Tetrapoda", "Perissodactyla,Artiodactyla,Aves,Embrithopoda,Proboscidea", "Perissodactyla,Artiodactyla,Aves,Embrithopoda,Proboscidea","Perissodactyla,Artiodactyla,Aves,Embrithopoda,Proboscidea,Carnivora"]
 
+let newWidth = $(".accordion").width() - 250;
+let activePanel = $(".accordion li.panel:first");
+
 animal.init = () => {
+    $(activePanel).addClass('active');
+    activePanel.focus();
     console.log('Getting Animal Data');
     for (let i = 0; i < time_periods.length; i++) {
         
@@ -89,25 +94,52 @@ animal.init = () => {
             console.log(time_periods[i].name, res);
             let img_id = res.records[0].img.substring(res.records[0].img.lastIndexOf(":")+1);
             console.log(img_id);
-            $(`.animal${i + 1} img`).attr("src", `http://paleobiodb.org/data1.2/taxa/thumb.png?id=${img_id}`);
-            $(`.animal${i + 1} h3`).append(res.records[0].nam);
-
-            // $.ajax({
-            //     url: "http://paleobiodb.org/data1.2/taxa/thumb.png",
-            //     format: "GET",
-            //     dataType: "png",
-            //     data: {
-            //         id: img_id
-            //     }
-            // }).then((res) => {
-               
-                
-            // });
+            $(`.panel-${i + 1} img`).attr("src", `http://paleobiodb.org/data1.2/taxa/thumb.png?id=${img_id}`);
+            $(`.panel-${i + 1} h3`).append(res.records[0].nam);
+            
+            // $(`.panel-${i + 1} panel-content`).append($img, $animal_name);
         });
     }
 }
 
+$(document).keydown(function(e){
+    if (e.keyCode == 39 && activePanel[0].attributes.class != `class="panel panel-10 active"`){
+        console.log(activePanel[0].attributes.class);
+        
+        resize(activePanel.next());
+    }
+    if (e.keyCode == 37 && activePanel[0].attributes.class.nodeValue != "panel panel-0 active") {
+        console.log(activePanel[0].attributes.class.nodeValue);
+        console.log(`class="panel panel-0 active"`);
+        
+        resize(activePanel.prev());
+    }
+})
+$(".accordion").on('click', '.panel', function () {
+    if (!$(this).is('.active')) {
+        resize(this);
+    };
+});
+const resize = (next_item) =>{
+    $(activePanel).animate({ width: "25px" }, 300);
+    $(next_item).animate({ width: newWidth }, 300);
+    $('.accordion .panel').removeClass('active');
+    $(next_item).addClass('active');
+    activePanel = $(".accordion li.panel.active");
+    activePanel.focus();
+    console.log(activePanel);
+    
+}
+//when the window is resized
+$(window).on("resize", function () {
+    // update all variables that you need
+    newWidth = $(".accordion").width() - 250;
+    $(".panel.active").animate({ width: newWidth }, 1);
+
+});
 // **********On page load**********
 $(function () {
+    console.log(activePanel);
+    
     animal.init();
 });
